2f5c79ec02b073c581270898451db0cb1f74046e
wjcheon <wonjoongcheon@gmail.com>
2021-09-22 20:53:56 +0900

    imageFromText

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def getFrequencyDictForText(sentence):
     # making dict for counting frequencies
     for text in sentence.split(" "):
         #if re.match("a|the|an|the|to|in|for|of|or|by|with|is|on|that|be", text):
+        print(text)
         if re.match("과연|설마|제발|정말|결코|모름지기|응당|어찌|아마|정녕|아무쪼록|하물며|그리고|그러나|그러므로|즉|곧|및|혹은|또는", text):
             continue
         val = tmpDict.get(text, 0)
@@ -31,11 +32,32 @@ def getFrequencyDictForText(sentence):
 def makeImage(text):
 
     font_path = r"D2CodingBold-Ver1.3.2-20180524-ligature.ttf"
-    mask_path = r"단어모음 (jpg)/단어 (png)/커피.jpeg"
+    mask_path = r"단어모음 (jpg)/단어 (png)/소아청소년과_1024.jpg"  # width 와 height가 일정 크기 이상이어야 글자가 많이 들어감
     mask = np.array(Image.open(mask_path))  # powerpoint -> export as jpg format
 
-    wc = WordCloud(font_path=font_path, background_color="white", max_words=1000, mask=mask)
+    wc = WordCloud(font_path=font_path, background_color="white", max_words=20000, mask=mask, min_font_size=2,
+                   max_font_size=300, random_state=42)
+
     wc.generate_from_frequencies(text)
+    wc.recolor(color_func=color_func, random_state=3)
+
+    # show
+    plt.imshow(wc, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+    print('working: MakeImage')
+
+
+def makeImageFromText(text):
+
+    font_path = r"D2CodingBold-Ver1.3.2-20180524-ligature.ttf"
+    mask_path = r"단어모음 (jpg)/단어 (png)/소아청소년과.jpg"
+    mask = np.array(Image.open(mask_path))  # powerpoint -> export as jpg format
+
+
+    wc = WordCloud(font_path=font_path, background_color="white", max_words=20000, mask=mask, min_font_size=2, max_font_size=300, random_state=42)
+    wc.generate_from_text(text)
+    wc.recolor(color_func=color_func, random_state=3)
 
     # show
     plt.imshow(wc, interpolation="bilinear")
@@ -46,10 +68,14 @@ def makeImage(text):
 
 # TEXT
 #f = open(r"poem_YDJ.txt",  'rt', encoding='UTF8')
-f = open(r"질문txt/커피.txt",  'rt', encoding='UTF8')
+f = open(r"질문txt/소아청소년과.txt",  'rt', encoding='UTF8')
 text = f.read()
 print(text)
 f.close()
 
+# Frequency mode
 word_dict_frequency = getFrequencyDictForText(text)
 makeImage(word_dict_frequency)
+
+# text mode
+#makeImageFromText(text)
